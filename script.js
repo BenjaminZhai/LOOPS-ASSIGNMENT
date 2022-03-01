21bd35a66f86374bc86f50b9cdabfc6fe7ff5638
Benjamin Z <benzhai21@gmail.com>
2022-03-01 11:38:52 -0700

    qwe

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ document.getElementById("two").addEventListener("click", second)
 document.getElementById("three").addEventListener("click", third)
 document.getElementById("four").addEventListener("click", fourth)
 document.getElementById("five").addEventListener("click", fifth)
+
+function first(){
+    
+    for(n=1; n<=500; n++){
+        console.log("I'm So Happy!")
+    }
+}
