0760ef8adf383235aa140d5e715c78be9f498696
Benjamin Z <benzhai21@gmail.com>
2022-03-01 11:35:17 -0700

    wad

---
+++ script.js
@@ -0,0 +1,7 @@
+// LOOPS FUN
+
+document.getElementById("one").addEventListener("click", first)
+document.getElementById("two").addEventListener("click", second)
+document.getElementById("three").addEventListener("click", third)
+document.getElementById("four").addEventListener("click", fourth)
+document.getElementById("five").addEventListener("click", fifth)
