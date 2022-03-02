7457e5d1dfc6520a3be02b0bed1491f6e25c41c2
Benjamin Z <benzhai21@gmail.com>
2022-03-02 10:31:28 -0700

    awd

--- script.js
+++ script.js
@@ -12,3 +12,40 @@ function first(){
         console.log("I'm So Happy!")
     }
 }
+
+function second(){
+
+    for(x=12; x<=800; x+=4){
+        console.log(x)
+    }
+}
+
+function third(){
+
+    for (y=55; y>=11; y-=2){
+        console.log(y)
+    }
+}
+
+function fourth(){
+    let sum = 0
+    for (z=5; z<=50; z++){
+        sum = sum + z
+    }
+
+    if (z=50){
+        console.log(sum)
+    }
+    
+}
+
+function fifth(){
+    let sum = 0
+    for (a=10; a<=100; a+=10){
+        sum = sum + a
+    }
+
+    if (a = 100){
+        console.log(sum)
+    }
+}
